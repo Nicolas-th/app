55d6089bcc6aa408a37f2a72854119e41ad98afe
Nicolas Thomas <nicolas.thomas92@gmail.com>
2014-11-30 17:04:24 +0100

    Variables dans les pages statiques

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
 class PagesController < ApplicationController
   def home
+  	@titre = "Accueil"
   end
 
   def contact
+  	@titre = "Page contact"
   end
+
+  def about
+  	@titre = "Page à propos"
+  end
+
 end
